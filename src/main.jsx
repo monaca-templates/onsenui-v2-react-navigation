a180ca0a7053c75c0f80033791d9907b7bf43fa1
エリス <ellis.bryan@gmail.com>
2016-07-13 19:49:26 +0900

    Updated template to support webpack changes.

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import 'react-hot-loader/patch';
+import {AppContainer} from 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Load Onsen UI library
+import 'onsenui';
+
+// Onsen UI Styling and Icons
+require('onsenui/stylus/blue-basic-theme.styl');
+require('onsenui/css/onsenui.css');
+
+import App from './App';
+
+const rootElement = document.getElementById('app');
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootElement
+);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <AppContainer>
+         <NextApp />
+      </AppContainer>,
+      rootElement
+    );
+  });
+}
