e8618d857d6f052ed8ab58d11e23b7a97b024913
Eric Corson <emccorson@gmail.com>
2019-05-14 15:30:41 +0100

    Add iPhone X support

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import 'react-hot-loader/patch';
 import {AppContainer} from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ons from 'onsenui';
 
 // Onsen UI Styling and Icons
 require('onsenui/css-components-src/src/onsen-css-components.css');
 require('onsenui/css/onsenui.css');
 
 import App from './App';
+
+if (ons.platform.isIPhoneX()) {
+  document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+  document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+}
 
 const rootElement = document.getElementById('app');
 ReactDOM.render(
